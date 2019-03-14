f7c18e3242db68fc2096a31cf7b6e5e6de65c690
allen-woods <allen.woods@flatironschool.com>
2019-03-13 17:03:42 -0700

    First commit.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+
+let var1      = 2;
+let var2      = 31;
+
+let multiply  = (var1 * var2);
+let random    = Math.random();
+let mod       = (34 % 5);
+let max       = Math.max(-5,2,1,7,20,8,3);
+
